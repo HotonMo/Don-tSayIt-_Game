05b3deb6f10a7cac9673ae97d3916ad039eabc0c
hotonMo1 <halharithi@waredat.sa>
2023-06-17 15:15:02 +0300

    Initial Commit

---
+++ Don'tSayIt!_Game/Don_tSayIt__GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Don_tSayIt__GameApp.swift
+//  Don'tSayIt!_Game
+//
+//  Created by hoton on 28/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Don_tSayIt__GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
